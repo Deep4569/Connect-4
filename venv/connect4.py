1e338c21e60b12a07112c7826ae9c75de4c0667d
Deep <42325107+Deep4569@users.noreply.github.com>
2020-05-24 00:56:34 -0400

    board update complete

--- venv/connect4.py
+++ venv/connect4.py
@@ -1,33 +1,64 @@
 import pygame
+import math
 
 BLUE = (0,0,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 YELLOW = (255,255,0)
-WINDOW_WIDTH = 700
-WINDOW_HEIGHT = 600
 COLUMNS = 7
 ROWS = 6
-CELL_WIDTH = WINDOW_WIDTH / COLUMNS
-CELL_HEIGHT = WINDOW_HEIGHT / ROWS
+SQUARESIZE = 100
+WINDOW_WIDTH = COLUMNS * SQUARESIZE
+WINDOW_HEIGHT = (ROWS+1) * SQUARESIZE
+RADIUS = int(SQUARESIZE/2 - 5)
 
-pygame.init()
 size = (WINDOW_WIDTH, WINDOW_HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Connect 4")
-screen.fill(BLACK)
 
-def drawGrid():
-    for x in range(WINDOW_WIDTH):
-        for y in range(WINDOW_HEIGHT):
-            rect = pygame.Rect(x*CELL_WIDTH, y*CELL_HEIGHT,
-                               CELL_WIDTH, CELL_HEIGHT)
-            pygame.draw.rect(screen, BLUE, rect, 1)
+board = [[0] * COLUMNS for _ in range(ROWS)]
 
+def drawGrid(board, game_started):
+    for col in range(COLUMNS):
+        for row in range(ROWS):
+            pygame.draw.rect(screen, BLUE, (col * SQUARESIZE, row * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (int(col * SQUARESIZE + SQUARESIZE / 2), int(row * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+
+    if game_started:
+        for col in range(COLUMNS):
+            for row in range(ROWS):
+                color = RED if board[row][col] == 1 else (YELLOW if board[row][col] == 2 else BLACK)
+                pygame.draw.circle(screen, color, (int(col * SQUARESIZE + SQUARESIZE / 2), WINDOW_HEIGHT - int(row * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+
+def place_token(board, col, player):
+    if board[ROWS-1][col] == 0:
+        for row in range(ROWS):
+            if board[row][col] == 0:
+                board[row][col] = player
+                break
+
+pygame.init()
+player = 0
+drawGrid(board, False)
 
 while True:
-    drawGrid()
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            pygame.quit()
-    pygame.display.flip()
+        if event.type == pygame.QUIT:
+            pygame.QUIT
+
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK, (0, 0, WINDOW_WIDTH, SQUARESIZE))
+            posx = event.pos[0]
+            if player == 0:
+                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
+            else:
+                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS)
+        pygame.display.update()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            posx = event.pos[0]
+            col = int(math.floor(posx/SQUARESIZE))
+            place_token(board, col, player+1)
+            drawGrid(board,True)
+
+            player = (player + 1) % 2
