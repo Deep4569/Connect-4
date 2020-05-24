06242ebfd3aa177dd565203372826327a3228467
Deep <42325107+Deep4569@users.noreply.github.com>
2020-05-24 01:24:34 -0400

    Game finished

--- venv/connect4.py
+++ venv/connect4.py
@@ -7,10 +7,10 @@ RED = (255,0,0)
 YELLOW = (255,255,0)
 COLUMNS = 7
 ROWS = 6
-SQUARESIZE = 100
-WINDOW_WIDTH = COLUMNS * SQUARESIZE
-WINDOW_HEIGHT = (ROWS+1) * SQUARESIZE
-RADIUS = int(SQUARESIZE/2 - 5)
+CELL_SIZE = 100
+WINDOW_WIDTH = COLUMNS * CELL_SIZE
+WINDOW_HEIGHT = (ROWS+1) * CELL_SIZE
+RADIUS = int(CELL_SIZE/2 - 5)
 
 size = (WINDOW_WIDTH, WINDOW_HEIGHT)
 screen = pygame.display.set_mode(size)
@@ -21,14 +21,14 @@ board = [[0] * COLUMNS for _ in range(ROWS)]
 def drawGrid(board, game_started):
     for col in range(COLUMNS):
         for row in range(ROWS):
-            pygame.draw.rect(screen, BLUE, (col * SQUARESIZE, row * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
-            pygame.draw.circle(screen, BLACK, (int(col * SQUARESIZE + SQUARESIZE / 2), int(row * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+            pygame.draw.rect(screen, BLUE, (col * CELL_SIZE, row * CELL_SIZE + CELL_SIZE, CELL_SIZE, CELL_SIZE))
+            pygame.draw.circle(screen, BLACK, (int(col * CELL_SIZE + CELL_SIZE / 2), int(row * CELL_SIZE + CELL_SIZE + CELL_SIZE / 2)), RADIUS)
 
     if game_started:
         for col in range(COLUMNS):
             for row in range(ROWS):
                 color = RED if board[row][col] == 1 else (YELLOW if board[row][col] == 2 else BLACK)
-                pygame.draw.circle(screen, color, (int(col * SQUARESIZE + SQUARESIZE / 2), WINDOW_HEIGHT - int(row * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+                pygame.draw.circle(screen, color, (int(col * CELL_SIZE + CELL_SIZE / 2), WINDOW_HEIGHT - int(row * CELL_SIZE + CELL_SIZE / 2)), RADIUS)
 
 def place_token(board, col, player):
     if board[ROWS-1][col] == 0:
@@ -37,28 +37,49 @@ def place_token(board, col, player):
                 board[row][col] = player
                 break
 
+def check_winning_move(board, player):
+    directions = [ [1,0], [1,-1], [1,1], [0,1] ]
+    for direction in directions:
+        dirx = direction[0]
+        diry = direction[1]
+
+        for col in range(COLUMNS):
+            for row in range(ROWS):
+                lastx = row + 3*dirx
+                lasty = col + 3*diry
+                if (0 <= lastx and lastx < COLUMNS and 0 <= lasty and lasty < ROWS):
+                    val = board[row][col];
+                    if (val is not 0 and val == board[row+dirx][col+diry]
+                                     and val == board[row+2 * dirx][col+2 * diry]
+                                     and val == board[lastx][lasty]): return True
+    return False
+
 pygame.init()
 player = 0
 drawGrid(board, False)
+game_finished = False
 
-while True:
+while not game_finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.QUIT
 
         if event.type == pygame.MOUSEMOTION:
-            pygame.draw.rect(screen, BLACK, (0, 0, WINDOW_WIDTH, SQUARESIZE))
+            pygame.draw.rect(screen, BLACK, (0, 0, WINDOW_WIDTH, CELL_SIZE))
             posx = event.pos[0]
             if player == 0:
-                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
+                pygame.draw.circle(screen, RED, (posx, int(CELL_SIZE / 2)), RADIUS)
             else:
-                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS)
+                pygame.draw.circle(screen, YELLOW, (posx, int(CELL_SIZE / 2)), RADIUS)
         pygame.display.update()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             posx = event.pos[0]
-            col = int(math.floor(posx/SQUARESIZE))
+            col = int(math.floor(posx/CELL_SIZE))
             place_token(board, col, player+1)
             drawGrid(board,True)
+            if check_winning_move(board, player+1):
+                print("PLAYER %s WINS" % str(player+1))
+                game_finished = True
 
             player = (player + 1) % 2
